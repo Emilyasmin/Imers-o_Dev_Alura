cc9f41dbaeb78eb9313ef8a9f9990dcef7ca77f9
Emily Yasmin <emilyyasmin91@gmail.com>
2023-02-03 16:06:27 -0300

    a

--- Mentalista/js/game.js
+++ Mentalista/js/game.js
@@ -4,76 +4,31 @@ let input = document.querySelector('input');
 let reposta = document.getElementById('resposta');
 
 function game() {
-  var section = document.querySelector('section');
   var numeroSecreto = parseInt(Math.random() * 1001);
-  let chute = parseInt(document.querySelector('input').value);
-  console.log(chute)
-  console.log(numeroSecreto)
+  var chute = parseInt(document.querySelector('input').value);
+  var section = document.querySelector('section');
+  var iterador = 0;
+  console.log(numeroSecreto);
 
+  //A cada vez que envia uma nova tentativa, executa novamete.Corrigir isso!!!
   while (chute != numeroSecreto) {
-    var ContadorTentativas = 1;
-    var tentativasPossiveis = 10;
-    if (chute > numeroSecreto) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é maior que '+chute+'</h1>';
-    } else if (chute < numeroSecreto) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é menor que '+chute+'</h1>';
-    } else if (chute == numeroSecreto) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Acertou!!!!</h1>';
+    if (iterador != 0) {
+      section.innerHTML = section.innerHTML + '<h1 id="reposta">Você já fez' + iterador + 'tentativas!</h1>';
     }
 
-    if (ContadorTentativas == tentativasPossiveis) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">!GAME OVER! O número que pensei foi ' + numeroSecreto + '</h1>';
-      break
+    if (chute < numeroSecreto) {
+      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é maior que ' + chute + '</h1>';
+    } else if (chute > numeroSecreto) {
+      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é menor que ' + chute + '</h1>';
     }
-    ContadorTentativas++;
+    iterador += 1;
+    break
+  }
+  if (chute == numeroSecreto) {
+    document.h1.innerHTML = " ";
+    section.innerHTML = section.innerHTML + '<h1 id="reposta">Acertou!!!!</h1>';
   }
 }
-
-
-const startTimer = () => {
-
-  this.loop = setInterval(() => {
-    const currentTime = +timer.innerHTML;
-    timer.innerHTML = currentTime + 1;
-  }, 10);
-
-}
-// const game = () => {
-// let numeroSecreto = parseInt(Math.random() * 1001);
-// let tentativasPossiveis = 10;
-// let ContadorTentativas = 1;
-
-//   while(chute != numeroSecreto){
-//     if (chute == numeroSecreto) {
-//       alert('Acertou!')
-//     } else if (chute > numeroSecreto) {
-//       alert('Quase lá... o número secreto é menor que ' + chute + '\n Você já tentou ' + ContadorTentativas + ' vezes')
-//     } else if (chute < numeroSecreto) {
-//       alert('Quase lá... o número secreto é maior que ' + chute + '\n Você já tentou ' + ContadorTentativas + ' vezes')
-//     }
-
-//     if (ContadorTentativas == tentativasPossiveis) {
-//       alert('!GAME OVER! \n' + 'O número que pensei foi ... ' + numeroSecreto)
-//       break
-//     }
-//     ContadorTentativas++
-//   }
-
-
-// }
-
-
-
-// const startTimer = () => {
-
-//   this.loop = setInterval(() => {
-//     const currentTime = +timer.innerHTML;
-//     timer.innerHTML = currentTime + 1;
-//   }, 1000);
-
-// }
-
-
 
 const validateInput = ({ target }) => {
   if (target.value.length > 0) {
@@ -84,7 +39,35 @@ const validateInput = ({ target }) => {
 }
 input.addEventListener('input', validateInput);
 
+
 window.onload = () => {
   nome.innerHTML = 'Bem vindo(a), ' + localStorage.getItem('player') + '!';
   game();
 }
+
+
+
+
+
+
+// var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1);
+// var iterador = 0;
+// console.log(numeroSecreto);
+// while (chute != numeroSecreto) {
+
+//   if (iterador != 0) {
+//     alert('Você já fez' + iterador + 'tentativas!')
+//   }
+
+//   var chute = prompt('Digite um número entre 1 e 1000');
+
+//   if (chute == numeroSecreto) {
+//     alert('Acertou!');
+//   } else if (chute > numeroSecreto) {
+//     alert('O número secreto é menor que ' + chute);
+//   } else if (chute < numeroSecreto) {
+//     alert('O número secreto é maior que ' + chute);
+//   }
+
+//   iterador += 1;
+// }
--- Mentalista/js/teste.js
+++ Mentalista/js/teste.js
@@ -1,86 +1,39 @@
-alert("Você só pode tentar 10 vezes");
+let nome = document.getElementById("player");
+let input = document.getElementById('chute');
 
+var button  = document.getElementById("button")
+var todosChutes = [ ];
 
-var numeroSecreto = parseInt(Math.random() * 1001);
-var chute = prompt("Mentalize um número de 0 a 1000");
-
-if (chute == numeroSecreto) {
-
-    alert("VOCÊ ACERTOU DE PRIMEIRA, THE BEST MENTALISTA");
-
+button.onclick = function(){
+  todosChutes.push(input.value); 
+  console.log(todosChutes);
+  return todosChutes;
 }
-var contador = 1;
-while (chute != numeroSecreto) {
-
-    if (chute > numeroSecreto) {
-
-        alert("Mentalizou errado, o " + chute + " não é o correto, ele é menor");
-
-    }else if (chute < numeroSecreto) {
-
-        alert("Mentalizou errado, o " + chute + " náo é o correto, ele é maior");
-
-    }
-
-    alert("Você tentou " + contador + " vezes");
-
-    if (contador == 10) {
-
-        break;
-
-    }
-
-    contador = contador + 1;
-
-
-    chute = prompt("Tente novamente meu caro mentalista");
-
-    if (chute == numeroSecreto) {
-
-        alert("Finalmente acertou, " + chute + " é o número certo");
-        alert("Você tentou " + contador + " vezes");
-
-    }
-
-}
-
-
-
-// var numSecreto = parseInt(Math.random() * 1001);
-// var quantChute = 0;
-// // se o chute for igual ao numero secreto alert de acertou
-
-// while(chute != numSecreto ){
-//   var chute = parseInt(prompt('Digite um número entre 0 e 1000'));
-//   var quantMaxima = 15;
   
-  
-//     if(chute == numSecreto ){
-//       alert('acertou!')
-      
-//     }else if(chute > numSecreto){
-//       alert('Errou! O número secreto é menor que ' + chute)
-  
-      
-//     }else if(chute < numSecreto){
-//       alert('Errou! o número secreto é maior que ' + chute)
-      
-//     }
-     
- 
-//     quantChute = quantChute + 1
-//     var conta = quantMaxima - quantChute;
-      
-  
-//   if (quantChute == 15){
-//     alert ('Você exedeu a quantidade de tentativas! O número era: ' + numSecreto);
-//     break
-    
-//   }else if(quantChute == 10){
-//     alert ('Faltam 5 tentativas');
-  
-//   }else{
-//     alert('Você já fez ' + quantChute + ' chutes!' + ' Te restam ' + conta);
-//   }
-  
+
+// var campo = document.querySelector("input")
+// var but = document.querySelector("button")
+// var medicamentos = [];
+
+// but.onclick = function(){
+//     medicamentos.push(campo.value);
+//     console.log(medicamentos);
 // }
+
+
+
+
+
+const validateInput = ({ target }) => {
+  if (target.value.length > 0) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', '');
+  }
+}
+input.addEventListener('input', validateInput);
+
+
+window.onload = () => {
+  nome.innerHTML = 'Bem vindo(a), ' + localStorage.getItem('player') + '!';
+}
