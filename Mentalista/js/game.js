946f6943fbd93af9bb0853be4f2f679aed6104e6
Emily Yasmin <emilyyasmin91@gmail.com>
2023-02-04 14:25:41 -0300

    Versão simplificada concluida

--- Mentalista/js/game.js
+++ Mentalista/js/game.js
@@ -1,72 +1,32 @@
 let nome = document.getElementById("player");
-let form = document.querySelector('form');
-let input = document.querySelector('input');
-let reposta = document.getElementById('resposta');
+let button = document.getElementById("button");
 
-function game() {
-  var numeroSecreto = parseInt(Math.random() * 1001);
-  var chute = parseInt(document.querySelector('input').value);
-  var section = document.querySelector('section');
-  var iterador = 0;
-  console.log(numeroSecreto);
 
-  while (chute != numeroSecreto) {
-    if (iterador != 0) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Você já fez' + iterador + 'tentativas!</h1>';
-    }
+button.onclick = function game(){
+let numeroAleatorio = Math.floor(Math.random() * (1001 - 1) + 1);
+let tentativasRestantes = 10;
 
-    if (chute < numeroSecreto) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é maior que ' + chute + '</h1>';
-    } else if (chute > numeroSecreto) {
-      section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é menor que ' + chute + '</h1>';
-    }
-    iterador += 1;
-    break
-  }
-  if (chute == numeroSecreto) {
-    document.h1.innerHTML = " ";
-    section.innerHTML = section.innerHTML + '<h1 id="reposta">Acertou!!!!</h1>';
-  }
-}
+while (tentativasRestantes > 0) {
+  let chute = prompt(`Tente adivinhar o número entre 1 e 1000. Você ainda tem ${tentativasRestantes} tentativas.`);
 
-const validateInput = ({ target }) => {
-  if (target.value.length > 0) {
-    button.removeAttribute('disabled');
+  if (chute == numeroAleatorio) {
+    alert(`Você acertou o número em ${tentativasRestantes} tentativas! Parabéns!`);
+    break;
+  } else if (chute < numeroAleatorio) {
+    alert("O número secreto é maior que "+ chute);
   } else {
-    button.setAttribute('disabled', '');
+    alert("O número secreto é menor que" + chute);
   }
+  tentativasRestantes--;
 }
-input.addEventListener('input', validateInput);
-
+if (tentativasRestantes === 0) {
+  alert(`!GAME OVER!\n\nO número correto era ${numeroAleatorio}.`);
+}
+}
 
 window.onload = () => {
-  nome.innerHTML = 'Bem vindo(a), ' + localStorage.getItem('player') + '!';
-  game();
-}
+  nome.innerHTML = 'Olá, ' + localStorage.getItem('player') + '!';
+};
 
 
 
-
-
-
-// var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1);
-// var iterador = 0;
-// console.log(numeroSecreto);
-// while (chute != numeroSecreto) {
-
-//   if (iterador != 0) {
-//     alert('Você já fez' + iterador + 'tentativas!')
-//   }
-
-//   var chute = prompt('Digite um número entre 1 e 1000');
-
-//   if (chute == numeroSecreto) {
-//     alert('Acertou!');
-//   } else if (chute > numeroSecreto) {
-//     alert('O número secreto é menor que ' + chute);
-//   } else if (chute < numeroSecreto) {
-//     alert('O número secreto é maior que ' + chute);
-//   }
-
-//   iterador += 1;
-// }
--- Mentalista/js/teste.js
+++ Mentalista/js/teste.js
@@ -3,28 +3,29 @@ let input = document.getElementById('chute');
 var button = document.getElementById("button")
 var todosChutes = [];
 
-
+//Validar se o input não está vazio
 function valida_input() {
   if (input.value.length < 0) {
-    alert('Por favor, preencha o campo input');
+    alert('Por favor, preencha o campo');
     input.focus();
     return false
   }
 }
+valida_input()
 
+// Armazenar o valor que foi colocado no input dentro de uma array
 button.onclick = async function armazena_chute() {
-  todosChutes.unshift(input.value)
-  console.log(todosChutes)
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 100)
-  })
+  if (valida_input == true) {
+    todosChutes.unshift(input.value)
+    console.log(todosChutes)
+  }
 }
 
+//Coletar o primeiro dado do array e retornar de acordo com as condições
 async function game() {
   await armazena_chute()
-  // var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1)
+  console.log("iep")
+ // var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1)
   // var iterador = 0
   // var section = document.querySelector('section')
   // chute = todosChutes[0]
@@ -43,8 +44,8 @@ async function game() {
   //   }
   //   iterador += 1;
   // }
-  console.log("iep")
 }
+game()
 
 const validateInput = ({ target }) => {
   if (target.value.length > 0) {
@@ -58,41 +59,24 @@ input.addEventListener('input', validateInput);
 
 window.onload = () => {
   nome.innerHTML = 'Bem vindo(a), ' + localStorage.getItem('player') + '!';
-  game()
 };
 
 
 
 
+ 
 
 
-
-
-
-
-
-
-
-
-
-// function game(){
-//   var numeroSecreto = parseInt(Math.random() * (1001 - 1) + 1)
-//   var iterador = 0
-//   var section = document.querySelector('section')
-//   chute = todosChutes[0]
-//   console.log(numeroSecreto)
-
-//   while (chute != numeroSecreto) {
-//     if (iterador != 0) {
-//       section.innerHTML = section.innerHTML + '<h1 id="reposta">Você já fez' + iterador + 'tentativas!</h1>';
-//     }
-//     if (chute == numeroSecreto) {
-//       section.innerHTML = section.innerHTML + '<h1 id="reposta">Acertou!!!!</h1>';
-//     }else if (chute < numeroSecreto) {
-//       section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é maior que ' + chute + '</h1>';
-//     } else if (chute > numeroSecreto) {
-//       section.innerHTML = section.innerHTML + '<h1 id="reposta">Quase lá! O número secreto é menor que ' + chute + '</h1>';
-//     }
-//     iterador += 1;
-//   }
-// }
+//Possivel HTMl para esse cod. em teste
+  // <main class="container">
+  //       <section class="content">
+  //           <h1 id="player"></h1>
+  //           <h1>Tente acertar em qual número estou pensando de 0 e 1000.</h1>
+  //           <h2>Pense com cuidado, te darei apenas 10 chances!</h2>
+  //           <form class="form">
+  //               <input id="chute" type="number" placeholder="Tente a sorte!"  required>
+  //               <button id="button" type="button" disabled>Tentar</button>
+  //           </form>
+  //          <h1 id="reposta"></h1> 
+  //       </section>
+  //   </main>
